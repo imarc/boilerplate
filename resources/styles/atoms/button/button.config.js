f761574440bee09a8f02cacead8b5244559a918b
Kevin Hamer <kevin@imarc.com>
2020-04-21 00:58:36 -0400

    wip - implement @use and start component cleanup

--- resources/styles/atoms/button/button.config.js
+++ resources/styles/atoms/button/button.config.js
@@ -1,6 +1,6 @@
 module.exports = {
     "collated": true,
     "collator": markup => {
-        return `<hr>${markup}`;
+        return `<div style="padding: 16px 16px 0;">${markup}</div>`;
     },
 }
