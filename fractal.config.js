4bd912c8292d1f0f46b0db0a97737c1dfee78c78
Kevin Hamer <kevin@imarc.com>
2021-01-20 15:50:21 -0500

    switch from consolidate to @frctl/twig

--- fractal.config.js
+++ fractal.config.js
@@ -1,16 +1,14 @@
 'use strict'
 
 const Path = require('path')
-const consolidate = require('@frctl/consolidate')
 const fractal = module.exports = require('@frctl/fractal').create()
-const twig = require('twig')
 const mandelbrot = require('@frctl/mandelbrot')
 const theme = require('fractal-theme-hydrogen')(mandelbrot)
+const twigAdapter = require('@frctl/twig')()
 
-twig.extendFunction('prefix', path => Path.join(process.env.RESOURCE_ROOT || '/', path))
-
-fractal.components.engine(consolidate('twig', twig.twig))
+fractal.components.engine(twigAdapter)
 fractal.components.set('ext', '.twig')
+fractal.web.theme(theme)
 
 fractal.components.set('statuses.deprecated', {
     label: 'Deprecated',
@@ -18,13 +16,15 @@ fractal.components.set('statuses.deprecated', {
     color: '#886600',
 })
 
-fractal.set('project.title', 'Design System')
-fractal.components.set('default.context', { scripts: [], })
-fractal.components.set('default.collator', markup => `<div style="margin:0 0 1em">${markup}</div>`)
+fractal.set('project.title', 'Pattern Library')
+//fractal.components.set('default.collator', markup => `<div style="margin:0 0 1em">${markup}</div>`)
 fractal.components.set('path', __dirname + '/resources/styles')
 fractal.docs.set('path', __dirname + '/resources/styles/docs')
 fractal.web.set('builder.dest', __dirname + '/web/pattern-library')
-fractal.web.set('builder.static.ignored', __dirname + '/web/pattern-library')
 fractal.web.set('static.path', __dirname + '/web')
-fractal.web.set('server.watch', true)
-fractal.web.theme(theme)
+fractal.web.set('builder.static.ignored', [
+    Path.join(__dirname, '/web/pattern-library'),
+    Path.join(__dirname, '/web/index.php')
+])
+fractal.web.set('server.sync', true)
+fractal.web.set('server.syncOptions', { watchOptions: { ignored: /\.scss$/i } });
