bdc1d8b4162714252032a392831d68f71d242419
Kevin Hamer <kevin@imarc.com>
2021-02-08 16:40:32 -0500

    ie11 fixes

--- resources/styles/molecules/card/card.config.js
+++ resources/styles/molecules/card/card.config.js
@@ -1,7 +1,7 @@
 module.exports = {
     collated: true,
     collator: (markup, item) =>
-        `<div style="margin-bottom: 3rem">
+        `<div style="margin: 0 3rem 3rem">
             <div style="margin-bottom: 1.5rem">${item.label}</div>
             ${markup}
          </div>`,
