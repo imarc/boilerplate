c33a5274ba837b51be1477244db6969755562171
Kevin Hamer <kevin@imarc.com>
2021-05-17 12:03:58 -0400

    adding swiper components

---
+++ resources/styles/organisms/carousel/BpCarousel.vue
@@ -0,0 +1,39 @@
+<template>
+    <swiper ref="mySwiper" :options="swiperOptions" class="foobar">
+        <slot v-for="(_, name) in $slots" :name="name" :slot="name" />
+    </swiper>
+</template>
+<script>
+import { Swiper, SwiperSlide, directive } from 'vue-awesome-swiper'
+import 'swiper/css/swiper.css'
+
+export default {
+    props: {
+        swiperOptions: {
+            type: Object,
+            default: () => ({
+                keyboard: {
+                    enabled: true,
+                },
+                navigation: {
+                    nextEl: '.swiper-button-next',
+                    prevEl: '.swiper-button-prev',
+                },
+                pagination: {
+                    el: '.swiper-pagination',
+                },
+
+                slidesPerView: 2.5,
+                spaceBetween: 40,
+            }),
+        },
+    },
+    components: {
+        Swiper,
+        SwiperSlide,
+    },
+    directives: {
+        swiper: directive,
+    },
+}
+</script>
