1876c3aeb932af2f5f643f7b8f8170384839f5e4
Kevin Hamer <kevin@imarc.com>
2021-05-17 17:10:42 -0400

    implement two carousels, including the bleed off only one side variant

--- resources/styles/organisms/carousel/BpCarousel.vue
+++ resources/styles/organisms/carousel/BpCarousel.vue
@@ -1,54 +1,76 @@
 <template>
-    <swiper ref="mySwiper" :options="swiperOptions" class="foobar">
-        <slot v-for="(_, name) in $slots" :name="name" :slot="name" />
+    <swiper
+        ref="mySwiper"
+        :options="swiperOptions"
+        class="foobar"
+    >
+        <slot
+            v-for="(_, name) in $slots"
+            :slot="name"
+            :name="name"
+        />
     </swiper>
 </template>
 <script>
-import { Swiper, SwiperSlide, directive } from 'vue-awesome-swiper'
+import { Swiper, directive } from 'vue-awesome-swiper'
 import 'swiper/css/swiper.css'
 
-export default {
-    props: {
-        swiperOptions: {
-            type: Object,
-            default: () => ({
-                keyboard: {
-                    enabled: true,
-                },
-                navigation: {
-                    nextEl: '.swiper-button-next',
-                    prevEl: '.swiper-button-prev',
-                },
-                pagination: {
-                    el: '.swiper-pagination',
-                },
+const variants = {
+    default: {
+        spaceBetween: 40,
+        centeredSlides: true,
+    },
+    bleedRight: {
+        slidesPerView: 1,
+        spaceBetween: 40,
 
-                slidesPerView: 1,
+        breakpoints: {
+            616: {
+                slidesPerView: 1.5,
                 spaceBetween: 40,
-
-                breakpoints: {
-                    616: {
-                        slidesPerView: 1.25,
-                        spaceBetween: 40,
-                    },
-                    1000: {
-                        slidesPerView: 1.5,
-                        spaceBetween: 40,
-                    },
-                    1200: {
-                        slidesPerView: 2.25,
-                        spaceBetween: 40,
-                    },
-                }
-            }),
+            },
+            1000: {
+                slidesPerView: 1.75,
+                spaceBetween: 40,
+            },
+            1200: {
+                slidesPerView: 2.25,
+                spaceBetween: 40,
+            },
         },
     },
+}
+
+export default {
     components: {
         Swiper,
-        SwiperSlide,
     },
     directives: {
         swiper: directive,
+    },
+    props: {
+        variant: {
+            type: String,
+            default: 'default',
+        },
+        swiperOptions: {
+            type: Object,
+            default () {
+                return {
+                    keyboard: {
+                        enabled: true,
+                    },
+                    navigation: {
+                        nextEl: '.swiper-button-next',
+                        prevEl: '.swiper-button-prev',
+                    },
+                    pagination: {
+                        el: '.swiper-pagination',
+                    },
+                    ...variants[this.variant],
+                }
+            },
+        },
     },
 }
 </script>
