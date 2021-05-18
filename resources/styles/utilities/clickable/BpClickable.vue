c98151b5281d72cfeeacc476c5e530fd60447b16
Kevin Hamer <kevin@imarc.com>
2021-05-17 22:47:46 -0400

    add bpclickable

---
+++ resources/styles/utilities/clickable/BpClickable.vue
@@ -0,0 +1,26 @@
+<script>
+export default {
+    methods: {
+        click (evt, link) {
+            if (evt.target.tagName == 'A') {
+                return
+            }
+
+            let url = null
+            if (typeof (link) === 'string') {
+                url = link
+            } else if (link instanceof Node) {
+                url = link.getAttribute('href')
+            }
+
+            evt.preventDefault()
+            window.location = url
+        },
+    },
+    render () {
+        return this.$scopedSlots.default({
+            click: this.click,
+        })
+    },
+}
+</script>
