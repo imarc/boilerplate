8681f32cb4723366e923c9b3a475e85985bc15a9
Kevin Hamer <kevin@imarc.com>
2022-03-01 15:31:11 -0500

    wip - trying to enable layer stiles for testimonial, unable to run watch anymore

--- resources/styles/organisms/callToActionSection/callToActionSection.config.js
+++ resources/styles/organisms/callToActionSection/callToActionSection.config.js
@@ -1,3 +1,7 @@
 module.exports = {
     label: 'Call to Action Section',
+    variantsName: 'Call to Action Section Modifiers',
+    collated: true,
+    variants: ['-secondary', '-lightGray']
+        .map(name => ({ name, label: name, context: { class: name } })),
 }
--- resources/styles/organisms/testimonialSection/testimonialSection.config.js
+++ resources/styles/organisms/testimonialSection/testimonialSection.config.js
@@ -1,3 +1,7 @@
 module.exports = {
     name: 'Testimonal Section',
+    variantsName: 'Call to Action Section Modifiers',
+    collated: true,
+    variants: ['-primary', '-secondary', '-lightGray']
+        .map(name => ({ name, label: name, context: { class: name } })),
 }
