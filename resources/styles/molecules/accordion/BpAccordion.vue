0c86c417dcf56cbaf86214d06d5385be73825227
Vincent <vincentnyamu@imarc.com>
2022-06-03 10:52:33 -0400

    updated bpAccordion style, transition, and code cleanup

--- resources/styles/molecules/accordion/BpAccordion.vue
+++ resources/styles/molecules/accordion/BpAccordion.vue
@@ -1,43 +1,40 @@
 <template>
-    <button :class="[block, isOpen ? '-open' : '-closed']">
+    <button :class='[block, isOpen ? "-open" : "-closed"]'>
         <div
-            :id="`${id}-control`"
-            :class="`${block}__${headerElement}`"
-            :aria-expanded="`${isOpen}`"
-            :aria-controls="id"
-            tabindex="0"
-            role="button"
-            @click="open"
-            @keyup.enter="open"
+            :id='`${id}-control`'
+            :class='`${block}__${headerElement}`'
+            :aria-expanded='`${isOpen}`'
+            :aria-controls='id'
+            tabindex='0'
+            role='button'
+            @click='open'
+            @keyup.enter='open'
         >
-            <slot name="heading" />
-            <slot name="icon">
+            <slot name='heading' />
+            <slot name='icon'>
                 <svg
-                    :class="`${block}__icon`"
-                    width="24"
-                    height="24"
-                    viewBox="0 0 24 24"
-                    stroke-width="2"
-                    stroke="currentColor"
-                    fill="none"
+                    :class='`${block}__icon`'
+                    width='24'
+                    height='24'
+                    viewBox='0 0 24 24'
+                    stroke-width='2'
+                    stroke='currentColor'
+                    fill='none'
                 >
-                    <polyline points="6 9 12 15 18 9" />
+                    <polyline points='6 9 12 15 18 9' />
                 </svg>
             </slot>
         </div>
         <Transition
-            name="accordion__transition"
-            @enter="startTransition"
-            @after-enter="endTransition"
-            @before-leave="startTransition"
-            @after-leave="endTransition"
+            name='accordion__transition'
+            :duration='1000'
         >
             <div
-                v-if="isOpen"
-                :id="id"
-                ref="content"
-                :class="`${block}__${contentElement}`"
-                :aria-labelledby="`${id}-control`"
+                v-if='isOpen'
+                :id='id'
+                ref='content'
+                :class='`${block}__${contentElement}`'
+                :aria-labelledby='`${id}-control`'
             >
                 <slot />
             </div>
@@ -46,39 +43,39 @@
 </template>
 
 <script setup>
-    import { ref } from "@vue/reactivity";
+import { ref } from 'vue'
 
-    defineProps({
-        block: {
-            type: String,
-            default: "accordion",
-        },
-        headerElement: {
-            type: String,
-            default: "header",
-        },
-        contentElement: {
-            type: String,
-            default: "content",
-        },
-        id: {
-            type: String,
-            default: () => Math.random().toString(36).substr(2),
-        },
-    });
+defineProps({
+    block: {
+        type: String,
+        default: 'accordion',
+    },
+    headerElement: {
+        type: String,
+        default: 'header',
+    },
+    contentElement: {
+        type: String,
+        default: 'content',
+    },
+    id: {
+        type: String,
+        default: () => Math.random().toString(36).substr(2),
+    },
+});
 
-    const isOpen = ref(false);
+const isOpen = ref(false);
 
-    const open = () => {
-        isOpen.value = !isOpen.value;
-    };
+const open = () => {
+    isOpen.value = !isOpen.value
+}
 
-    function startTransition(el, done) {
-        el.style.height = el.scrollHeight + "px";
-    };
-    function endTransition(el, done) {
-        el.style.height = "";
-        done()
-    };
-
+function startTransition(el, done) {
+    el.style.maxHeight = 100 + 'vh'
+    el.style.padding = '0.5rem 0'
+}
+function endTransition(el, done) {
+    el.style.maxHeight = 0
+    done()
+}
 </script>
